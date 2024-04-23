b64d233b2385cfd99fc6c5626f5a42be897b6a5e
Fabian Weik <fabian.weik@protonmail.com>
2024-04-23 22:09:43 +0200

    read periods file

--- examples/logistic/src/main.rs
+++ examples/logistic/src/main.rs
@@ -3,6 +3,7 @@ use rant::{
     simulate::SimulationOptions,
 };
 
+#[derive(Debug)]
 struct Parameters {
     a: f64,
 }
@@ -49,9 +50,31 @@ fn main() {
 
 #[cfg(test)]
 mod test {
+    use anyhow::anyhow;
+    use rant::util::tna::read_tna_periods_file;
+
+    use super::*;
+
+    fn unproject_initial_state_and_parameters(
+        values: Vec<f64>,
+    ) -> anyhow::Result<(f64, Parameters)> {
+        if values.len() == 1 {
+            Ok((0.5, Parameters { a: values[0] }))
+        } else {
+            Err(anyhow!("expected exactly 1 value"))
+        }
+    }
+
     #[test]
     fn period_test() {
         let max_period = 128;
         let iterations = 20_000;
+
+        let periods = read_tna_periods_file(
+            "ant/test_data/period.tna",
+            unproject_initial_state_and_parameters,
+        );
+        dbg!(periods);
+        assert!(false)
     }
 }
--- src/util/tna.rs
+++ src/util/tna.rs
@@ -1,11 +1,13 @@
 use std::fs::read_to_string;
 
+use thiserror::Error;
+
 pub fn read_tna_file(path: &str) -> anyhow::Result<Vec<Vec<f64>>> {
     let content = read_to_string(path)?;
 
     let lines = content.lines().filter(|line| {
         let line_content: String = line.chars().filter(|c| !c.is_whitespace()).collect();
-        !(line_content.is_empty() || line_content.starts_with("#"))
+        !(line_content.is_empty() || line_content.starts_with('#'))
     });
 
     Ok(lines
@@ -15,4 +17,24 @@ pub fn read_tna_file(path: &str) -> anyhow::Result<Vec<Vec<f64>>> {
                 .collect::<Result<Vec<f64>, _>>()
         })
         .collect::<Result<Vec<Vec<f64>>, _>>()?)
+}
+
+#[derive(Debug, Error)]
+enum TnaError {
+    #[error("too few values in line")]
+    TooFewValues,
+}
+
+pub fn read_tna_periods_file<S, P>(
+    path: &str,
+    unproject_initial_state_and_parameters: impl Fn(Vec<f64>) -> anyhow::Result<(S, P)>,
+) -> anyhow::Result<Vec<(S, P, usize)>> {
+    read_tna_file(path)?
+        .into_iter()
+        .map(|mut line| -> anyhow::Result<(S, P, usize)> {
+            let period = line.pop().ok_or(TnaError::TooFewValues)?;
+            let (state, parameters) = unproject_initial_state_and_parameters(line)?;
+            Ok((state, parameters, period as usize))
+        })
+        .collect::<anyhow::Result<Vec<_>>>()
 }
