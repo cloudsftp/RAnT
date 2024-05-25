1424f65a93980a87a35f7ad3711c7c9802473e01
Fabian Weik <fabian.weik@protonmail.com>
2024-05-25 16:59:43 +0200

    fix complex number squared

--- examples/mandelbrot/src/complex.rs
+++ examples/mandelbrot/src/complex.rs
@@ -1,6 +1,6 @@
 use std::ops::Add;
 
-#[derive(Debug, Clone, Copy, Default)]
+#[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub struct C {
     pub a: f64,
     pub b: f64,
@@ -14,7 +14,7 @@ impl C {
     pub fn square(self) -> Self {
         Self {
             a: self.a.powi(2) + self.b.powi(2),
-            b: 2. * (self.a + self.b),
+            b: 2. * self.a * self.b,
         }
     }
 
@@ -30,6 +30,35 @@ impl Add for C {
         Self::Output {
             a: self.a + rhs.a,
             b: self.b + rhs.b,
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    struct SquareTestCase {
+        input: C,
+        output: C,
+    }
+
+    #[test]
+    fn test_square() {
+        let tests = vec![
+            SquareTestCase {
+                input: C::new(0., 0.),
+                output: C::new(0., 0.),
+            },
+            SquareTestCase {
+                input: C::new(1., 0.),
+                output: C::new(1., 0.),
+            },
+        ];
+
+        for (i, test) in tests.iter().enumerate() {
+            let output = test.input.square();
+            assert_eq!(output, test.output, "test case {}", i);
         }
     }
 }
