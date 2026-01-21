f80bf3e83855f9ae05355d2741177e44af8065c6
Fabian Weik <fabian.weik@protonmail.com>
2026-01-21 20:10:14 +0100

    update logistic example

--- examples/logistic/src/main.rs
+++ examples/logistic/src/main.rs
@@ -1,9 +1,13 @@
 #[cfg(test)]
 mod test;
 
-use rantlib::simulate::period::Cycle;
+use rantlib::{
+    output::text::write_results,
+    scan::{adapters::ParameterAdapter1DEven, generators::VectorGenerator1D, scan},
+    simulate::period::{simulate, Cycle, SimulationOptions},
+};
 
-use std::cmp::Ordering;
+use std::{cmp::Ordering, fmt::format};
 
 #[derive(Debug)]
 struct Parameters {
@@ -26,14 +30,11 @@ fn compare_states(a: &f64, b: &f64) -> Ordering {
     a.total_cmp(b)
 }
 
-const START: f64 = 3.;
-const END: f64 = 4.;
-const RESOLUTION: usize = 100;
 fn construct_parameters(a: f64) -> (f64, Parameters) {
     (0.5, Parameters { a })
 }
 
-fn project_results(_: &f64, parameters: &Parameters, result: &Cycle<f64>) -> Option<String> {
+fn project_results_period(_: f64, parameters: Parameters, result: Cycle<f64>) -> Option<String> {
     let period = match &result {
         Cycle::FixedPoint(_) => 1,
         Cycle::Cycle(cycle) => cycle.len(),
@@ -43,31 +44,55 @@ fn project_results(_: &f64, parameters: &Parameters, result: &Cycle<f64>) -> Opt
     Some(format!("{:} {}", parameters.a, period))
 }
 
-fn main() {
-    /*
-    let max_period = 128;
-    let iterations = 20_000;
-    let delta = 1e-9;
-
-    let scan_options = ScanOptions {
-        resolutions: vec![RESOLUTION],
+fn project_results_limit_object(
+    _: f64,
+    parameters: Parameters,
+    result: Cycle<f64>,
+) -> Option<String> {
+    let object = match &result {
+        Cycle::FixedPoint(x) => format!("(1) {x}"),
+        Cycle::Cycle(cycle) => format!("({}) {:?}", cycle.len(), cycle),
+        Cycle::Divergence => format!("diverged"),
     };
 
-    let simulation_options = SimulationOptions {
-        iterations,
-        max_period,
-        delta,
-    };
+    Some(format!("{}: {}", parameters.a, object))
+}
 
-    let result = scan(
+const MAX_PERIOD: usize = 128;
+const ITERATIONS: usize = 20_000;
+const DELTA: f64 = 1e-9;
+
+fn simulate_logistic(x: f64, parameters: &Parameters) -> Cycle<f64> {
+    simulate(
+        x,
+        parameters,
         logistic,
         distance,
-        compute_parameters,
-        scan_options,
-        simulation_options,
-    );
+        SimulationOptions {
+            iterations: ITERATIONS,
+            max_period: MAX_PERIOD,
+            delta: DELTA,
+        },
+    )
+}
 
-    write_results(result, project_results, "period.tnar")
+const START: f64 = 3.;
+const END: f64 = 4.;
+const RESOLUTION: usize = 100;
+
+fn main() {
+    let vector_generator = VectorGenerator1D {
+        resolution: RESOLUTION,
+    };
+
+    let parameter_adapter = ParameterAdapter1DEven {
+        start: START,
+        end: END,
+        construct_initial_state_and_parameters: |a| (0.5, Parameters { a }),
+    };
+
+    let result = scan(vector_generator, parameter_adapter, simulate_logistic);
+
+    write_results(result, project_results_limit_object, "period.tnar")
         .expect("something went wrong while writing");
-    */
 }
